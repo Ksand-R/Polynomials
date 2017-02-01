f7459ae47197cd2335fecb775b1e57de5d3cfeac
Alexander Romanov <al.romanov2012@yandex.ru>
2017-02-01 17:55:48 +0300

    Added Input/ output for polynomials; Revorked class Monom

--- Polynomials/Polynomials/Polynomial.h
+++ Polynomials/Polynomials/Polynomial.h
@@ -1,41 +1,88 @@
 #pragma once
+#include <iostream>
 #include "Monom.h"
-
-//struct Monom_list
-//{
-//	Monom mon_;
-//	Monom* next;
-//};
-
+using namespace std;
 
 
 class Polynomial {
 private:
-	Monom* mon_ptr;
+	Monom* head;
+	Monom* tail;
+	int size; // counter of amount of monoms
 
-	Polynomial(char* str) {
-		//долго писать, потом, запуш € этим займусь, это по анологии, ка» € в польскую запись переводил, только тут чутка по другому
-	}
+public:
 
-	void Print_poly(Monom* mon_ptr_) {
-		Monom* pointer = mon_ptr;
-		while (pointer->next != 0) {
-			printf("%lf x^%i y^%i z^%i", pointer->coef, (*pointer)., pointer.deg_y, pointer.deg_z);//не понимаю как степень вызвать
-			if ( (pointer->next != 0) && (pointer->coef > 0) ) {
-				printf(" + ");
+	Polynomial() { size = 0; head = tail = NULL; }
+
+	void Print_poly() {
+		Monom* temp = head;
+
+		while (head) {
+			cout << temp->coef << "x^" 
+				<< temp->deg / 10000 << "y^" 
+				<< temp->deg / 100 % 100 << "z^" 
+				<< temp->deg % 100;
+
+			if ( (temp->next) && (temp->coef > 0) ) {
+				printf("+");
 			}
-			pointer = pointer->next;
+
+			temp = temp->next;
 		}
 	}
 
-	void Scan_poly(char* str);
 
-	Polynomial operator - (Monom m);
-	Polynomial operator + (Monom m);
-	Polynomial operator * (Monom m);
+
+	void Input_poly() {
+		int f = -1;
+		int curr_coef = -1;
+		int curr_x = -1;
+		int curr_y = -1;
+		int curr_z = -1;
+		
+		while (f != 1) {
+			Monom* new_node = new Monom();
+
+			cout << "Enter another coefficient: " << endl;
+			cin >> curr_coef;
+			new_node->coef = curr_coef;
+
+			cout << "enter enother degree of x: " << endl;
+			cin >> curr_x;
+			new_node->deg = curr_x * 10000;
+
+			cout << "enter enother degree of y: " << endl;
+			cin >> curr_y;
+			new_node->deg += curr_y * 100;
+
+			cout << "enter enother degree of z: " << endl;
+			cin >> curr_z;
+			new_node->deg += curr_z;
+
+			new_node->next = NULL;
+
+			if (tail == NULL)
+			{
+				head = tail = new_node;
+				tail->next = NULL;
+			}
+			else {
+				tail->next = new_node;
+				tail = new_node;
+				size++;
+			}
+			cout << "Is Input ended? // 0 means no, 1 means yes " << endl;
+			cin >> f;
+			
+
+		}
+	}
+
+	//Polynomial operator - (Monom m);
+	//Polynomial operator + (Monom m);
+	//Polynomial operator * (Monom m);
 
 	~Polynomial() {
-		//чЄ то придЄтс€ удалать
 	}
 
 };
